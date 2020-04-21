2f56eaef3f1bd5156f7ae8df0017c0efd8327df8
Alvin Mammoottil <monsialvin@gmail.com>
2020-04-21 11:29:18 +0930

    made donate page

---
+++ script.js
@@ -0,0 +1,14 @@
+function myAlert() {
+  alert("Your Donation has been Submitted");
+}
+var loadFile = function(event) {
+  var output = document.getElementById('output');
+  output.src = URL.createObjectURL(event.target.files[0]);
+  output.onload = function() {
+    URL.revokeObjectURL(output.src)
+  }
+};
+$(document).ready(function(){
+  $("#choosefile").click(function(){
+    $("#choosefile").hide();
+  })});
